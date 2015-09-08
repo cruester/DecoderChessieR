d3055a65c261447ffed775f8fc9c3279d3939e92
Chessie Ruester <cruester@e104-m-06.mchenry.edu>
2015-09-08 12:32:44 -0500

    Initial commit

---
+++ decodeprogram/src/main/java/com/example/Decode.java
@@ -0,0 +1,13 @@
+package com.example;
+
+public class Decode
+{
+    public static void main(String[] args)
+    {
+        char sunCode = '\u2600';
+        char umbrellaCode = '\u2602';
+
+        System.out.println("I wish the " + sunCode + " would come out so I no longer needed my "
+                + umbrellaCode);
+    }
+}
